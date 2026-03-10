6b972d4fa5b30b18e85b60e26bd3223355786093
a1ex <ilzavfxx@gmail.com>
2026-03-10 22:54:27 +0000

    Implement basic Discord bot with user reply

--- bot.py
+++ bot.py
@@ -1,1 +1,26 @@
+import discord
+import os
 
+TARGET_USER_ID = 1443023498338373703  # replace with the user ID
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"Logged in as {client.user}")
+
+@client.event
+async def on_message(message):
+
+    if message.author.bot:
+        return
+
+    if message.author.id == TARGET_USER_ID:
+        await message.reply("you're a dork")
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+client.run(TOKEN)
