15189ee5a8e9f7a93373a0fc7b37b43bb6df87dd
a1ex <ilzavfxx@gmail.com>
2026-03-10 23:19:39 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord
 import os
 
-TARGET_USER_ID = 525129432927698944  # replace with the user ID
+TARGET_USER_ID = 875803669830971482  # replace with the user ID
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -19,7 +19,7 @@ async def on_message(message):
         return
 
     if message.author.id == TARGET_USER_ID:
-        await message.reply("yoo beebs hush up")
+        await message.reply("https://cdn.discordapp.com/attachments/1349551267906850929/1480287940440625162/am3jvo.gif?ex=69b1c413&is=69b07293&hm=a1ff5e041941b78ea4e78bf243083860b07fdec362cad6762714092df64c032c&")
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 
